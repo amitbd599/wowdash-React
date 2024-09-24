238da0087c23a694894dec2a7e4f05f2cc8d4af2
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-09-24 12:34:38 +0600

    UserActivatesOne add

--- master/src/hook/useReactApexChart.js
+++ master/src/hook/useReactApexChart.js
@@ -1395,9 +1395,57 @@ const useReactApexChart = () => {
         }
     };
 
+    let statisticsDonutChartSeriesThree = [30, 25]
+
+    var statisticsDonutChartOptionsThree = {
+
+        colors: ['#FF9F29', '#487FFF'],
+        labels: ['Female', 'Male'],
+        legend: {
+            show: false
+        },
+        chart: {
+            type: 'donut',
+            height: 260,
+            sparkline: {
+                enabled: true // Remove whitespace
+            },
+            margin: {
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0
+            },
+            padding: {
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0
+            }
+        },
+        stroke: {
+            width: 0,
+        },
+        dataLabels: {
+            enabled: false
+        },
+        responsive: [{
+            breakpoint: 480,
+            options: {
+                chart: {
+                    width: 200
+                },
+                legend: {
+                    position: 'bottom'
+                }
+            }
+        }],
+    };
 
 
-    return { chartSeries, chartOptions, barChartSeries, barChartOptions, donutChartSeries, donutChartOptions, paymentStatusChartSeries, paymentStatusChartOptions, barChartSeriesTwo, barChartOptionsTwo, donutChartSeriesTwo, donutChartOptionsTwo, paymentStatusChartSeriesTwo, paymentStatusChartOptionsTwo, createChart, createChartTwo, createChartThree, createChartFour, paymentStatusChartSeriesThree, paymentStatusChartOptionsThree, statisticsDonutChartSeries, statisticsDonutChartOptions, candleStickChartSeries, candleStickChartOptions };
+
+
+    return { chartSeries, chartOptions, barChartSeries, barChartOptions, donutChartSeries, donutChartOptions, paymentStatusChartSeries, paymentStatusChartOptions, barChartSeriesTwo, barChartOptionsTwo, donutChartSeriesTwo, donutChartOptionsTwo, paymentStatusChartSeriesTwo, paymentStatusChartOptionsTwo, createChart, createChartTwo, createChartThree, createChartFour, paymentStatusChartSeriesThree, paymentStatusChartOptionsThree, statisticsDonutChartSeries, statisticsDonutChartOptions, candleStickChartSeries, candleStickChartOptions, statisticsDonutChartSeriesThree, statisticsDonutChartOptionsThree };
 };
 
 export default useReactApexChart;
