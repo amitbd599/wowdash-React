5860f1f624d8ceae6f3bfcb6c7b8494d187d2634
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-10-04 19:34:33 +0600

    Radar Chart add

--- master/src/components/child/RadarChart.jsx
+++ master/src/components/child/RadarChart.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import useReactApexChart from '../../hook/useReactApexChart'
+import ReactApexChart from 'react-apexcharts'
 
 const RadarChart = () => {
+
+    let { radarChartSeries, radarChartOptions } = useReactApexChart()
     return (
         <div className="col-md-6">
             <div className="card h-100 p-0">
@@ -8,10 +12,10 @@ const RadarChart = () => {
                     <h6 className="text-lg fw-semibold mb-0">Radar Chart</h6>
                 </div>
                 <div className="card-body p-24 text-center">
-                    <div
-                        id="radarChart"
-                        className="square-marker check-marker series-gap-24 d-flex justify-content-center"
-                    />
+
+                    <ReactApexChart id="radarChart"
+                        className="square-marker check-marker series-gap-24 d-flex justify-content-center" options={radarChartOptions} series={radarChartSeries} type="radar"
+                        height={264} />
                 </div>
             </div>
         </div>
--- master/src/hook/useReactApexChart.js
+++ master/src/hook/useReactApexChart.js
@@ -3579,6 +3579,45 @@ const useReactApexChart = () => {
     };
 
 
+    let radarChartSeries = [{
+        name: 'Product 1',
+        data: [80, 50, 30, 40, 60, 20, 62, 30, 40, 80],
+    }, {
+        name: 'Product 2',
+        data: [80, 60, 80, 70, 68, 60, 56, 50, 40, 45],
+    }]
+    var radarChartOptions = {
+
+        colors: ['#FF9F29', '#487FFF'],
+        chart: {
+            height: 264,
+            type: 'radar',
+            toolbar: {
+                show: false,
+            },
+            dropShadow: {
+                enabled: true,
+                blur: 1,
+                left: 1,
+                top: 1
+            }
+        },
+        stroke: {
+            width: 2
+        },
+        fill: {
+            opacity: 0.25
+        },
+        markers: {
+            size: 0
+        },
+        yaxis: {
+            stepSize: 20
+        },
+        xaxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
+        }
+    };
 
 
 
@@ -3586,7 +3625,9 @@ const useReactApexChart = () => {
 
 
 
-    return { chartSeries, chartOptions, barChartSeries, barChartOptions, donutChartSeries, donutChartOptions, paymentStatusChartSeries, paymentStatusChartOptions, barChartSeriesTwo, barChartOptionsTwo, donutChartSeriesTwo, donutChartOptionsTwo, paymentStatusChartSeriesTwo, paymentStatusChartOptionsTwo, createChart, createChartTwo, createChartThree, createChartFour, createChartFive, createChartSix, createChartSeven, createChartEight, createChartNine, paymentStatusChartSeriesThree, paymentStatusChartOptionsThree, statisticsDonutChartSeries, statisticsDonutChartOptions, candleStickChartSeries, candleStickChartOptions, statisticsDonutChartSeriesThree, statisticsDonutChartOptionsThree, upDownBarChartSeries, upDownBarChartOptions, semiCircleGaugeSeriesOne, semiCircleGaugeOptionsOne, dailyIconBarChartSeriesOne, dailyIconBarChartOptionsOne, transactionLineChartSeries, transactionLineChartOptions, userOverviewDonutChartSeries, userOverviewDonutChartOptions, paymentStatusChartSeriesOne, paymentStatusChartOptionsOne, dailyIconBarChartSeriesTwo, dailyIconBarChartOptionsTwo, columnChartSeriesOne, columnChartOptionsOne, columnChartSeriesTwo, columnChartOptionsTwo, columnChartSeriesThree, columnChartOptionsThree, columnChartSeriesFour, columnChartOptionsFour, defaultLineChartSeries, defaultLineChartOptions, zoomAbleLineChartSeries, zoomAbleLineChartOptions, lineDataLabelSeries, lineDataLabelOptions, doubleLineChartSeries, doubleLineChartOptions, stepLineChartSeries, stepLineChartOptions, timeSeriesChartSeries, timeSeriesChartOptions, pieChartSeriesOne, pieChartOptionsTwo, basicDonutChartSeries, basicDonutChartOptions };
+
+
+    return { chartSeries, chartOptions, barChartSeries, barChartOptions, donutChartSeries, donutChartOptions, paymentStatusChartSeries, paymentStatusChartOptions, barChartSeriesTwo, barChartOptionsTwo, donutChartSeriesTwo, donutChartOptionsTwo, paymentStatusChartSeriesTwo, paymentStatusChartOptionsTwo, createChart, createChartTwo, createChartThree, createChartFour, createChartFive, createChartSix, createChartSeven, createChartEight, createChartNine, paymentStatusChartSeriesThree, paymentStatusChartOptionsThree, statisticsDonutChartSeries, statisticsDonutChartOptions, candleStickChartSeries, candleStickChartOptions, statisticsDonutChartSeriesThree, statisticsDonutChartOptionsThree, upDownBarChartSeries, upDownBarChartOptions, semiCircleGaugeSeriesOne, semiCircleGaugeOptionsOne, dailyIconBarChartSeriesOne, dailyIconBarChartOptionsOne, transactionLineChartSeries, transactionLineChartOptions, userOverviewDonutChartSeries, userOverviewDonutChartOptions, paymentStatusChartSeriesOne, paymentStatusChartOptionsOne, dailyIconBarChartSeriesTwo, dailyIconBarChartOptionsTwo, columnChartSeriesOne, columnChartOptionsOne, columnChartSeriesTwo, columnChartOptionsTwo, columnChartSeriesThree, columnChartOptionsThree, columnChartSeriesFour, columnChartOptionsFour, defaultLineChartSeries, defaultLineChartOptions, zoomAbleLineChartSeries, zoomAbleLineChartOptions, lineDataLabelSeries, lineDataLabelOptions, doubleLineChartSeries, doubleLineChartOptions, stepLineChartSeries, stepLineChartOptions, timeSeriesChartSeries, timeSeriesChartOptions, pieChartSeriesOne, pieChartOptionsTwo, basicDonutChartSeries, basicDonutChartOptions, radarChartSeries, radarChartOptions };
 };
 
 export default useReactApexChart;
