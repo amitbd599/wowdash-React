14aa2eb95f5838754900fdb905be974e20c49663
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-10-05 12:16:20 +0600

    HalfStar add

--- master/src/components/StarRatingLayer.jsx
+++ master/src/components/StarRatingLayer.jsx
@@ -1,6 +1,7 @@
 import { Icon } from '@iconify/react/dist/iconify.js'
 import React from 'react'
 import DefaultStarRatings from './child/DefaultStarRatings'
+import HalfStar from './child/HalfStar'
 
 const StarRatingLayer = () => {
     return (
@@ -9,32 +10,10 @@ const StarRatingLayer = () => {
             {/* DefaultStarRatings */}
             <DefaultStarRatings />
 
-            <div className="col-sm-6">
-                <div className="card h-100 p-0">
-                    <div className="card-header border-bottom bg-base py-16 px-24">
-                        <h6 className="text-lg fw-semibold mb-0">Half Star</h6>
-                    </div>
-                    <div className="card-body p-24">
-                        <ul className="d-flex flex-wrap align-items-center gap-12">
-                            <li className="text-warning-600 text-2xxl line-height-1">
-                                <Icon icon="material-symbols:star" />
-                            </li>
-                            <li className="text-warning-600 text-2xxl line-height-1">
-                                <Icon icon="material-symbols:star" />
-                            </li>
-                            <li className="text-warning-600 text-2xxl line-height-1">
-                                <Icon icon="material-symbols:star" />
-                            </li>
-                            <li className="text-warning-600 text-2xxl line-height-1">
-                                <Icon icon="mdi:star-outline" />
-                            </li>
-                            <li className="text-warning-600 text-2xxl line-height-1">
-                                <Icon icon="mdi:star-outline" />
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            {/* HalfStar */}
+            <HalfStar />
+
+
             <div className="col-sm-6">
                 <div className="card h-100 p-0">
                     <div className="card-header border-bottom bg-base py-16 px-24">
