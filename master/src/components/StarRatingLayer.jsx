fada19890daf7d36ffac726d783c57fc3a0faa68
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-10-05 12:17:08 +0600

    MultiColorStar add

--- master/src/components/StarRatingLayer.jsx
+++ master/src/components/StarRatingLayer.jsx
@@ -2,6 +2,7 @@ import { Icon } from '@iconify/react/dist/iconify.js'
 import React from 'react'
 import DefaultStarRatings from './child/DefaultStarRatings'
 import HalfStar from './child/HalfStar'
+import MultiColorStar from './child/MultiColorStar'
 
 const StarRatingLayer = () => {
     return (
@@ -14,32 +15,11 @@ const StarRatingLayer = () => {
             <HalfStar />
 
 
-            <div className="col-sm-6">
-                <div className="card h-100 p-0">
-                    <div className="card-header border-bottom bg-base py-16 px-24">
-                        <h6 className="text-lg fw-semibold mb-0">Multi Color</h6>
-                    </div>
-                    <div className="card-body p-24">
-                        <ul className="d-flex flex-wrap align-items-center gap-12">
-                            <li className="text-2xxl line-height-1 text-primary-600">
-                                <Icon icon="material-symbols:star" />
-                            </li>
-                            <li className="text-2xxl line-height-1 text-lilac-600">
-                                <Icon icon="material-symbols:star" />
-                            </li>
-                            <li className="text-2xxl line-height-1 text-success-600">
-                                <Icon icon="material-symbols:star" />
-                            </li>
-                            <li className="text-2xxl line-height-1 text-info-600">
-                                <Icon icon="material-symbols:star" />
-                            </li>
-                            <li className="text-2xxl line-height-1 text-danger-600">
-                                <Icon icon="material-symbols:star" />
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            {/* MultiColorStar */}
+            <MultiColorStar />
+
+
+
             <div className="col-sm-6">
                 <div className="card h-100 p-0">
                     <div className="card-header border-bottom bg-base py-16 px-24">
