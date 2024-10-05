c31bdbdfeb41b6f829efac8a6d6c33d3a09d3cea
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-10-05 13:40:07 +0600

    VerticalNavTabs add

--- master/src/components/child/VerticalNavTabs.jsx
+++ master/src/components/child/VerticalNavTabs.jsx
@@ -1,50 +1,28 @@
 import React from 'react'
-import DefaultTabs from './child/DefaultTabs'
-import FocusTabs from './child/FocusTabs'
-import ButtonTabs from './child/ButtonTabs'
-import ButtonTabsTwo from './child/ButtonTabsTwo'
-import VerticalNavTabs from './child/VerticalNavTabs'
 
-const TabsLayer = () => {
+const VerticalNavTabs = () => {
     return (
-        <div className="row gy-4">
-
-            {/* DefaultTabs */}
-            <DefaultTabs />
-
-            {/* FocusTabs */}
-            <FocusTabs />
-
-            {/* ButtonTabs */}
-            <ButtonTabs />
-
-            {/* ButtonTabsTwo */}
-            <ButtonTabsTwo />
-
-            {/* VerticalNavTabs */}
-            <VerticalNavTabs />
-
-
-
-
-            <div className="col-xxl-6">
-                <div className="card p-0 overflow-hidden position-relative radius-12 h-100">
-                    <div className="card-header pt-16 pb-0 px-24 bg-base border border-end-0 border-start-0 border-top-0 d-flex align-items-center flex-wrap justify-content-between">
-                        <h6 className="text-lg mb-0">Card Header Tabs</h6>
+        <div className="col-xxl-6">
+            <div className="card p-0 overflow-hidden position-relative radius-12 h-100">
+                <div className="card-header py-16 px-24 bg-base border border-end-0 border-start-0 border-top-0">
+                    <h6 className="text-lg mb-0">Vertical Nav Tabs</h6>
+                </div>
+                <div className="card-body p-24 pt-10">
+                    <div className="d-flex align-items-start">
                         <ul
-                            className="nav bordered-tab d-inline-flex nav-pills mb-0"
-                            id="pills-tab-six"
+                            className="nav button-tab nav-pills mb-16"
+                            id="pills-tab-five"
                             role="tablist"
                         >
                             <li className="nav-item" role="presentation">
                                 <button
-                                    className="nav-link px-16 py-10 active"
-                                    id="pills-header-home-tab"
+                                    className="nav-link fw-semibold text-primary-light radius-4 px-16 py-10 active"
+                                    id="pills-vertical-home-tab"
                                     data-bs-toggle="pill"
-                                    data-bs-target="#pills-header-home"
+                                    data-bs-target="#pills-vertical-home"
                                     type="button"
                                     role="tab"
-                                    aria-controls="pills-header-home"
+                                    aria-controls="pills-vertical-home"
                                     aria-selected="true"
                                 >
                                     Home
@@ -52,13 +30,13 @@ const TabsLayer = () => {
                             </li>
                             <li className="nav-item" role="presentation">
                                 <button
-                                    className="nav-link px-16 py-10"
-                                    id="pills-header-details-tab"
+                                    className="nav-link fw-semibold text-primary-light radius-4 px-16 py-10"
+                                    id="pills-vertical-details-tab"
                                     data-bs-toggle="pill"
-                                    data-bs-target="#pills-header-details"
+                                    data-bs-target="#pills-vertical-details"
                                     type="button"
                                     role="tab"
-                                    aria-controls="pills-header-details"
+                                    aria-controls="pills-vertical-details"
                                     aria-selected="false"
                                 >
                                     Details
@@ -66,13 +44,13 @@ const TabsLayer = () => {
                             </li>
                             <li className="nav-item" role="presentation">
                                 <button
-                                    className="nav-link px-16 py-10"
-                                    id="pills-header-profile-tab"
+                                    className="nav-link fw-semibold text-primary-light radius-4 px-16 py-10"
+                                    id="pills-vertical-profile-tab"
                                     data-bs-toggle="pill"
-                                    data-bs-target="#pills-header-profile"
+                                    data-bs-target="#pills-vertical-profile"
                                     type="button"
                                     role="tab"
-                                    aria-controls="pills-header-profile"
+                                    aria-controls="pills-vertical-profile"
                                     aria-selected="false"
                                 >
                                     Profile
@@ -80,111 +58,109 @@ const TabsLayer = () => {
                             </li>
                             <li className="nav-item" role="presentation">
                                 <button
-                                    className="nav-link px-16 py-10"
-                                    id="pills-header-settings-tab"
+                                    className="nav-link fw-semibold text-primary-light radius-4 px-16 py-10"
+                                    id="pills-vertical-settings-tab"
                                     data-bs-toggle="pill"
-                                    data-bs-target="#pills-header-settings"
+                                    data-bs-target="#pills-vertical-settings"
                                     type="button"
                                     role="tab"
-                                    aria-controls="pills-header-settings"
+                                    aria-controls="pills-vertical-settings"
                                     aria-selected="false"
                                 >
                                     Settings
                                 </button>
                             </li>
                         </ul>
-                    </div>
-                    <div className="card-body p-24 pt-10">
-                        <div className="tab-content" id="pills-tabContent-six">
+                        <div className="tab-content" id="pills-tab-fiveContent">
                             <div
                                 className="tab-pane fade show active"
-                                id="pills-header-home"
+                                id="pills-vertical-home"
                                 role="tabpanel"
-                                aria-labelledby="pills-header-home-tab"
+                                aria-labelledby="pills-vertical-home-tab"
                                 tabIndex={0}
                             >
-                                <div>
+                                <div className="flex-grow-1">
                                     <h6 className="text-lg mb-8">Title</h6>
                                     <p className="text-secondary-light mb-16">
                                         Lorem Ipsum&nbsp;is simply dummy text of the printing and
                                         typesetting industry. Lorem Ipsum has been the industry's
                                         standard dummy text ever since the 1500s, when an unknown
-                                        printer took a galley of type and scrambled it to make a type
-                                        specimen book. It has survived not{" "}
+                                        printer took a galley of type and scrambled it to make a type{" "}
                                     </p>
                                     <p className="text-secondary-light mb-0">
-                                        It was popularised in the 1960s with the release of Letraset
-                                        sheets containing Lorem Ipsum passages, and more recently with
-                                        desktop
+                                        &nbsp;It was popularised in the 1960s with the release of
+                                        Letraset sheets containing Lorem Ipsum passages, and more
+                                        recently with desktopLorem Ipsum&nbsp;is simply dummy text of
+                                        the printing and typesetting industry.{" "}
                                     </p>
                                 </div>
                             </div>
                             <div
                                 className="tab-pane fade"
-                                id="pills-header-details"
+                                id="pills-vertical-details"
                                 role="tabpanel"
-                                aria-labelledby="pills-header-details-tab"
+                                aria-labelledby="pills-vertical-details-tab"
                                 tabIndex={0}
                             >
-                                <div>
+                                <div className="flex-grow-1">
                                     <h6 className="text-lg mb-8">Title</h6>
                                     <p className="text-secondary-light mb-16">
                                         Lorem Ipsum&nbsp;is simply dummy text of the printing and
                                         typesetting industry. Lorem Ipsum has been the industry's
                                         standard dummy text ever since the 1500s, when an unknown
-                                        printer took a galley of type and scrambled it to make a type
-                                        specimen book. It has survived not{" "}
+                                        printer took a galley of type and scrambled it to make a type{" "}
                                     </p>
                                     <p className="text-secondary-light mb-0">
-                                        It was popularised in the 1960s with the release of Letraset
-                                        sheets containing Lorem Ipsum passages, and more recently with
-                                        desktop
+                                        &nbsp;It was popularised in the 1960s with the release of
+                                        Letraset sheets containing Lorem Ipsum passages, and more
+                                        recently with desktopLorem Ipsum&nbsp;is simply dummy text of
+                                        the printing and typesetting industry.{" "}
                                     </p>
                                 </div>
                             </div>
                             <div
                                 className="tab-pane fade"
-                                id="pills-header-profile"
+                                id="pills-vertical-profile"
                                 role="tabpanel"
-                                aria-labelledby="pills-header-profile-tab"
+                                aria-labelledby="pills-vertical-profile-tab"
                                 tabIndex={0}
                             >
-                                <div>
+                                <div className="flex-grow-1">
                                     <h6 className="text-lg mb-8">Title</h6>
                                     <p className="text-secondary-light mb-16">
                                         Lorem Ipsum&nbsp;is simply dummy text of the printing and
                                         typesetting industry. Lorem Ipsum has been the industry's
                                         standard dummy text ever since the 1500s, when an unknown
-                                        printer took a galley of type and scrambled it to make a type
-                                        specimen book. It has survived not{" "}
+                                        printer took a galley of type and scrambled it to make a type{" "}
                                     </p>
                                     <p className="text-secondary-light mb-0">
-                                        It was popularised in the 1960s with the release of Letraset
-                                        sheets containing Lorem Ipsum passages, and more recently with
-                                        desktop
+                                        &nbsp;It was popularised in the 1960s with the release of
+                                        Letraset sheets containing Lorem Ipsum passages, and more
+                                        recently with desktopLorem Ipsum&nbsp;is simply dummy text of
+                                        the printing and typesetting industry.{" "}
                                     </p>
                                 </div>
                             </div>
                             <div
                                 className="tab-pane fade"
-                                id="pills-header-settings"
+                                id="pills-vertical-settings"
                                 role="tabpanel"
-                                aria-labelledby="pills-header-settings-tab"
+                                aria-labelledby="pills-vertical-settings-tab"
                                 tabIndex={0}
                             >
-                                <div>
+                                <div className="flex-grow-1">
                                     <h6 className="text-lg mb-8">Title</h6>
                                     <p className="text-secondary-light mb-16">
                                         Lorem Ipsum&nbsp;is simply dummy text of the printing and
                                         typesetting industry. Lorem Ipsum has been the industry's
                                         standard dummy text ever since the 1500s, when an unknown
-                                        printer took a galley of type and scrambled it to make a type
-                                        specimen book. It has survived not{" "}
+                                        printer took a galley of type and scrambled it to make a type{" "}
                                     </p>
                                     <p className="text-secondary-light mb-0">
-                                        It was popularised in the 1960s with the release of Letraset
-                                        sheets containing Lorem Ipsum passages, and more recently with
-                                        desktop
+                                        &nbsp;It was popularised in the 1960s with the release of
+                                        Letraset sheets containing Lorem Ipsum passages, and more
+                                        recently with desktopLorem Ipsum&nbsp;is simply dummy text of
+                                        the printing and typesetting industry.{" "}
                                     </p>
                                 </div>
                             </div>
@@ -193,8 +169,7 @@ const TabsLayer = () => {
                 </div>
             </div>
         </div>
-
     )
 }
 
-export default TabsLayer
+export default VerticalNavTabs
